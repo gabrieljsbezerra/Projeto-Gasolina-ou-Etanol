c537fdaa706fc14efdbf11abae535eeaaccc9381
Gabriel João da Silva Bezerra <gabriel_jb30@hotmail.com>
2023-07-08 13:09:18 -0300

    Adicionando as Imagens no JS
    Adicionei as imagens no código de JS.

--- Arquivos_download/Projeto_Gasolina ou Álcool/script_gasoualc.js
+++ Arquivos_download/Projeto_Gasolina ou Álcool/script_gasoualc.js
@@ -5,17 +5,23 @@ function calcular() {
     var vetn = Number(etanol.value)
     var opc = document.getElementsByName('radopc')
     var res = document.getElementById('res')
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
 
     if (opc[0].checked) {
         divisao = vetn / vgas
         if (divisao < 0.70) {
             usar = 'Etanol / Álcool'
             document.body.style.background = '#79B939'
+            img.setAttribute('src', 'etanol.png')
         } else {
             usar = 'Gasolina'
             document.body.style.background = '#F97330'
+            img.setAttribute('src', 'gasolina.png')
         }
-    }   res.style.textAlign = 'center'
-        res.innerHTML = `A melhor opção de abastecimento é ${usar}!`
-        res.style.padding = 8
+    }
+    res.style.textAlign = 'center'
+    res.innerHTML = `A melhor opção de abastecimento é ${usar}!`
+    res.style.padding = 8
+    res.appendChild(img)
 }
